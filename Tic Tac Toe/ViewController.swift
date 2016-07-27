f16284612a46e458620806c696279a9ea6f64568
Nima M.Farahani <nima_f101@yahoo.com>
2016-07-26 23:52:08 -0400

    added functionality
    now can click on each square once

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    var activePlayer = 1
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        if gameState[sender.tag] == 0{
+            var image = UIImage()
+            
+            //change state of the pressed buttons
+            gameState[sender.tag] = activePlayer
+            if activePlayer == 1{
+                image = UIImage(named:("O.png"))!
+                activePlayer = 2
+
+            }else{
+                image = UIImage(named:("X.png"))!
+                activePlayer = 1
+
+            }
+            
+            // here we use sender.setImage instead of button.setImage
+            //because we want whichever button pressed, the same
+            //button changes to other photo
+            sender.setImage(image, forState: .Normal)
+            
+            //shows the tag for each button
+            //print(sender.tag)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
