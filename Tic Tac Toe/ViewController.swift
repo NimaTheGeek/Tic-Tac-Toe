b068137cb023c9e0f55b6f52c1099d1d2270a6e1
Nima M.Farahani <nima_f101@yahoo.com>
2016-07-31 18:47:31 -0400

    few bug fixes
    bugs

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
 
     @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var winner: UITextView!
-    
+    @IBOutlet weak var winner: UILabel!
+
     var activePlayer = 1
     var gameActive = true
     var gameState = [0,0,0,0,0,0,0,0,0]
     var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
     @IBAction func buttonPressed(sender: AnyObject) {
-        
         if gameState[sender.tag] == 0 && gameActive == true{
             var image = UIImage()
             
@@ -47,25 +50,49 @@ class ViewController: UIViewController {
                 if gameState[set[0]] != 0 && gameState[set[0]] == gameState[set[1]] && gameState[set[1]] == gameState[set[2]]{
                     if activePlayer == 1{
                         winner.text = "The winner is X"
+                        
                     }else{
                         winner.text = "The winner is O"
                     }
+                    
+                    // set the animation for blinking text
+                    UIView.animateWithDuration(0.5, delay: 1.0, options: [.CurveEaseInOut, .Autoreverse, .Repeat], animations: {
+                        
+                        self.winner.alpha = 0.0
+                        
+                        }, completion: nil)
+                    
                     gameActive = false
                 }
             }
         }
     }
     
+    // to make the button look round or oval add line layer.cornerRadius and make the corresponding 
+    // number more than 1, in Identity inpector section
+    
+    
+    @IBAction func reset(sender: AnyObject) {
+        activePlayer = 1
+        gameActive = true
+        gameState = [0,0,0,0,0,0,0,0,0]
+        var buttonReset : UIButton
+        for i in 0  ..< 9  {
+            buttonReset = view.viewWithTag(i) as! UIButton
+            buttonReset.setImage(nil, forState: .Normal)
+        }
+        winner.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
